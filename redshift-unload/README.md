d7d6b6f176fb8b2f44ef76a811b5fdee2097a02b
Anirudh Ramanathan <anirudh@rockset.com>
2018-09-13 18:02:08 -0700

    modified unload.py and readme

--- redshift-unload/README.md
+++ redshift-unload/README.md
@@ -26,16 +26,55 @@ from your environment.
 }
 ```
 
-# Install the python dependencies by running:
+# Install the python dependency by running:
 
 ```
-pip install -r requirements.txt
+pip install -U psycopg2
 ```
 If pip is not installed, use the following instructions: https://pip.pypa.io/en/stable/installing/
 
+# Configure your Redshift cluster and AWS credentials/
+
+Edit the config.json to add credentials to access your RDS cluster.
+Only the fields encapsulated in `<>` need to be modified.
+
+```
+{
+    "db": {
+        "host": "<redshift-cluster>",
+        "port": "<port>",
+        "database": "<database-name>",
+        "user": "<user>",
+        "password": "<password>"
+    },
+    "aws_access_key_id": "<key>",
+    "aws_secret_access_key": "<secret>",
+    "unload_options": [
+    	"ADDQUOTES",
+    	"PARALLEL ON",
+    	"ALLOWOVERWRITE",
+    	"DELIMITER ','",
+        "NULL AS '_rockset_null'"
+    ]
+}
+```
 
 # Run the script
 
+```
+usage: unload.py [-h] [-t T] [-c C] [-f F] [-r R] [-r1 R1] [-r2 R2]
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -t T        Table name
+  -c C        Schema name
+  -f F        Desired S3 file path
+  -r R        Range column
+  -r1 R1      Range start
+  -r2 R2      Range end
+```
+
+## Common usage:
 ```
 python ./unload.py -t <table-name> -f s3://<your-s3-bucket>/
 ```
--- redshift-unload/unload.py
+++ redshift-unload/unload.py
@@ -1,76 +1,96 @@
 '''
 Based on https://github.com/openbridge/ob_redshift_unload
-Turns nulls into _rockset_null
+Rockset Document 
 '''
 
 import json
 import os
 import psycopg2
 import argparse
+import yaml
+from pprint import pprint
+
+mapping = {
+    "INTEGER": ("SMALLINT", "INT2", "INTEGER", "INT", "INT4", "BIGINT", "DECIMAL"),
+    "STRING": ("CHAR", "CHARACTER", "NCHAR", "BPCHAR", "VARCHAR", "CHARACTER VARYING", "NVARCHAR", "TEXT"),
+    "FLOAT": ("REAL", "FLOAT4", "DOUBLE PRECISION", "FLOAT8", "FLOAT"),
+    "BOOLEAN": ("BOOLEAN", "BOOL"),
+    "DATETIME": ("TIMESTAMP", "TIMESTAMP WITHOUT TIME ZONE"),
+    "DATE": ("DATE"),
+    "TIMESTAMP": ("TIMESTAMPTZ", "TIMESTAMP WITH TIME ZONE")
+}
+
+def _get_rockset_type(redshift_type):
+    for key in mapping:
+        if redshift_type.upper() in mapping[key]:
+            return key
+    # if all else fails, call it a string.
+    return "STRING"
+
 
 def _simple_sanitize(s):
     return s.split(';')[0]
 
-def run(config, tablename, file_path, schema_name=None, sql_file=None, range_col=None, range_start=None, range_end=None):
-    if not file_path:
-        file_path = tablename
-    conn = psycopg2.connect(**config['db'])
-    unload_options = '\n'.join(config.get('unload_options', []))
-    cursor = conn.cursor()
+def _read_schema(cursor, schema_name, table_name):
     if schema_name:
-        query = "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{}' AND table_schema = '{}' ORDER BY ordinal_position".format(tablename, schema_name)
+        query = "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{}' AND table_schema = '{}' ORDER BY ordinal_position".format(table_name, schema_name)
     else:
-        query = "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{}' ORDER BY ordinal_position".format(tablename)
+        query = "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{}' ORDER BY ordinal_position".format(table_name)
     cursor.execute(query)
     res = cursor.fetchall()
+    return res
+
+def _dump_schema_file(schema_def, table_name):
+    file = 'schema.yaml'
+    serialized = dict()
+    serialized['version'] = 1.0
+    serialized[table_name] = list()
+    for v, t in schema_def:
+        serialized[table_name].append({v: _get_rockset_type(t)})
+    with open(file, 'w') as outfile:
+        yaml.dump(serialized, outfile, default_flow_style=False)
+    print('=== Completed write to {} ==='.format(file))
+
+def run(config, table_name, file_path, schema_name=None, range_col=None, range_start=None, range_end=None):
+    if not file_path:
+        file_path = table_name
+    conn = psycopg2.connect(**config['db'])
+    res = _read_schema(conn.cursor(), schema_name, table_name)
+    _dump_schema_file(res, table_name)
 
     cast_columns = []
-    columns = [x[0] for x in res]
     for col in res:
         if 'boolean' in col[1]:
             cast_columns.append("CASE {} WHEN 1 THEN \\\'true\\\' ELSE \\\'false\\\'::text END".format(col[0]))
         else:
             cast_columns.append("{}::text".format(col[0]))
-
-    header_str = ''
-    for i in columns:
-        header_str += "\\\'" + i + "\\\' as " + i.split(':')[0] + ', '
-    header_str = header_str.rstrip().rstrip(',')
-    column_str = ", ".join(columns)
     cast_columns_str = ", ".join(cast_columns)
 
     cursor = conn.cursor()
     where_clause = ""
+    unload_options = '\n'.join(config.get('unload_options', []))
     if range_col and range_start and range_end:
         where_clause = cursor.mogrify("WHERE {} BETWEEN \\\'{}\\\' AND \\\'{}\\\'".format(range_col, range_start, range_end,))
-    elif sql_file:
-        where_clause = sql_file
     query = """
-    UNLOAD (\'SELECT {0} FROM (
-        SELECT 1 as rn, {1} 
-        UNION ALL
-        (SELECT 2 as rn, {2} 
-        FROM {3}{4} {5})) ORDER BY rn\')  
-    TO \'{8}\'
-    CREDENTIALS 'aws_access_key_id={6};aws_secret_access_key={7}'
-    {9}
-    """.format(column_str, header_str, cast_columns_str, '{}.'.format(schema_name) if schema_name else '', tablename, 
+    UNLOAD (\'SELECT {0} FROM {1}{2} {3}\')  
+    TO \'{6}\'
+    CREDENTIALS 'aws_access_key_id={4};aws_secret_access_key={5}'
+    {7}
+    """.format(cast_columns_str, '{}.'.format(schema_name) if schema_name else '', table_name, 
                where_clause, config['aws_access_key_id'], 
                config['aws_secret_access_key'], file_path, unload_options)
     print("The following UNLOAD query is being run: \n" + query)
     cursor.execute(query)
-    print('Completed write to {}'.format(file_path))
+    print('=== Completed write to {} ==='.format(file_path))
 
 if __name__ == '__main__':
     config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json')
     with open(config_path, 'r') as f:
         config = json.loads(f.read())
-
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', help='Table name')
     parser.add_argument('-c', help='Schema name')
     parser.add_argument('-f', help='Desired S3 file path')
-    parser.add_argument('-s', help='SQL WHERE clause')
     parser.add_argument('-r', help='Range column')
     parser.add_argument('-r1', help='Range start')
     parser.add_argument('-r2', help='Range end')
@@ -84,4 +104,4 @@ if __name__ == '__main__':
             args[k] = _simple_sanitize(v)
         else:
             args[k] = None
-    run(config, args['t'], args['f'], args['c'], args['s'], args['r'], args['r1'], args['r2'])
+    run(config, args['t'], args['f'], args['c'], args['r'], args['r1'], args['r2'])
